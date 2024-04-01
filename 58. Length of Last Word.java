921eec8bc36d3dca9580fcacbf2816ec33e1ec2f
Iggy Spolski <58820214+Ignisko@users.noreply.github.com>
2024-04-01 08:35:05 +0200

    Create 58. Length of Last Word.java
    #add solution

---
+++ 58. Length of Last Word.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        int count = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ' ') {
+                if (count == 0) continue; // Skip trailing spaces
+                else break; // End of the last word
+            } else {
+                count++;
+            }
+        }
+        return count;
+    }
+}
